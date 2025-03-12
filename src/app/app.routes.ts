a56bbf1ce6898be0a4957739aba9e2a69689133f
monica <rojascelis10@gmail.com>
2025-03-11 22:18:55 -0500

    se realizo,la creacion de carpetas, componentes,modulos, el enlace de enrutamiento del login y registro, se instalo primeng

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "autenticacion", loadChildren:() => import("./module/auth/auth.module").then(moduleAuth=>moduleAuth.AuthModule)},
+    {path: "", redirectTo: "autenticacion/registro", pathMatch: "full"},
+];
