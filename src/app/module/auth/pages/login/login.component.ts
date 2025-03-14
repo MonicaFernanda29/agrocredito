ece8562054efc6412e100f0b855e80f6b6b07602
monica <rojascelis10@gmail.com>
2025-03-13 20:57:30 -0500

    se realizo la creacion del diseno de la pagina login y registro

--- src/app/module/auth/pages/login/login.component.ts
+++ src/app/module/auth/pages/login/login.component.ts
@@ -1,11 +1,18 @@
+import { InputTextModule } from 'primeng/inputtext';
+import { CardModule } from 'primeng/card';
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true, // Al ser un componente independiente
+  imports: [CommonModule, FormsModule ,CardModule, InputTextModule, ButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
 }
+
--- src/app/module/auth/pages/register/register.component.ts
+++ src/app/module/auth/pages/register/register.component.ts
@@ -1,11 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [CommonModule, FormsModule ,CardModule, InputTextModule, ButtonModule, DropdownModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  user = {
+    fullName: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    phone: '',
+    address: '',
+    userType: ''
+  };
+
+  userTypes = [
+    { label: 'Agricultor', value: 'agricultor' },
+    { label: 'Proveedor', value: 'proveedor' }
+  ];
+
+  onSubmit() {
+    if (this.user.password !== this.user.confirmPassword) {
+      alert('Las contraseñas no coinciden');
+      return;
+    }
+    alert('Registro exitoso');
+    console.log(this.user);
+  }
 
 }
